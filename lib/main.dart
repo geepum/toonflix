7742a0ee62ed7cc577e2c683832db51194a808b6
geepum <geepum@outlook.kr>
2023-02-05 17:45:21 +0900

    3.2 Buttons Section

--- lib/main.dart
+++ lib/main.dart
@@ -12,42 +12,90 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color.fromARGB(255, 3, 255, 163),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const SizedBox(
-                          height: 80,
+        backgroundColor: const Color.fromARGB(255, 3, 255, 163),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const SizedBox(
+                        height: 80,
+                      ),
+                      Text(
+                        'Hey, Terry',
+                        style: TextStyle(
+                          color: Colors.blueGrey.shade900,
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.w600,
                         ),
-                        Text(
-                          'Terry',
-                          style: TextStyle(
-                            color: Colors.blueGrey.shade900,
-                            fontSize: 40.0,
-                            fontWeight: FontWeight.w600,
-                          ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.black54.withOpacity(0.7),
+                          fontSize: 25.0,
                         ),
-                        Text(
-                          'June',
-                          style: TextStyle(
-                            color: Colors.black54.withOpacity(0.7),
-                            fontSize: 25.0,
-                          ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  color: Colors.black54.withOpacity(0.8),
+                  fontSize: 22,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 44,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45.0),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 50.0,
+                        vertical: 20.0,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 22.0,
                         ),
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ),
-          )),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
