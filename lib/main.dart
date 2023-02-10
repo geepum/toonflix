1f55e1d40c559d82ac0a37282cf6437df948cf8f
geepum <geepum@outlook.kr>
2023-02-10 21:00:04 +0900

    unfinished 3.7 Icons and Transforms

--- lib/main.dart
+++ lib/main.dart
@@ -133,43 +133,53 @@ class MyApp extends StatelessWidget {
                     vertical: 20.0,
                     horizontal: 25.0,
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text(
-                        'Euro',
-                        style: TextStyle(
-                          color: Colors.white70,
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10.0,
-                      ),
-                      Row(
-                        children: const [
-                          Text(
-                            '6 000',
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Euro',
                             style: TextStyle(
                               color: Colors.white70,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w600,
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.w900,
                             ),
                           ),
-                          SizedBox(
-                            width: 15.0,
+                          const SizedBox(
+                            height: 10.0,
                           ),
-                          Text(
-                            'EUR',
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w400,
-                            ),
+                          Row(
+                            children: const [
+                              Text(
+                                '6 000',
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 15.0,
+                              ),
+                              Text(
+                                'EUR',
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
-                      )
+                      ),
+                      const Icon(
+                        Icons.euro_rounded,
+                        color: Colors.white,
+                        size: 88,
+                      ),
                     ],
                   ),
                 ),
