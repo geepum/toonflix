cd8c3706a049c871d103a7fecb8b8bac110c1af4
geepum <geepum@outlook.kr>
2023-02-16 22:08:13 +0900

    4.1 setState

--- lib/main.dart
+++ lib/main.dart
@@ -1,198 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/Widgets/buttons.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 10;
+
+  void counterPlus() {
+    setState(() {
+      counter += 1;
+    });
+  }
+
+  void counterMinus() {
+    setState(() {
+      counter -= 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey[900],
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 80.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: const [
-                      Text(
-                        'Hey, Terry',
-                        style: TextStyle(
-                          color: Colors.white70,
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      Text(
-                        'Welcome back!',
-                        style: TextStyle(
-                          color: Colors.white70,
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 50.0,
-              ),
-              Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        'Total Balance',
-                        style: TextStyle(
-                          color: Colors.white70,
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      Text(
-                        '\$5 000 000',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Button(
-                      buttonColor: Color.fromRGBO(255, 179, 0, 1),
-                      buttonText: 'Transfer'),
-                  Button(
-                      buttonColor: Color.fromRGBO(158, 158, 158, 1),
-                      buttonText: 'Request'),
-                ],
-              ),
-              const SizedBox(
-                height: 50.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: const [
-                  Text(
-                    'Wallets',
-                    style: TextStyle(
-                      color: Colors.white70,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
-                  Text(
-                    'View All',
-                    style: TextStyle(
-                      color: Colors.white70,
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              Container(
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  color: Colors.grey[700],
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 20.0,
-                    horizontal: 25.0,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Euro',
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 25.0,
-                              fontWeight: FontWeight.w900,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10.0,
-                          ),
-                          Row(
-                            children: const [
-                              Text(
-                                '6 000',
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 15.0,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              SizedBox(
-                                width: 15.0,
-                              ),
-                              Text(
-                                'EUR',
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 15.0,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      Transform.translate(
-                        offset: const Offset(10.0, 10.0),
-                        child: Transform.scale(
-                          scale: 2.2,
-                          child: const Icon(
-                            Icons.euro_rounded,
-                            color: Colors.white,
-                            size: 60.0,
-                          ),
-                        ),
-                      ),
-                    ],
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Click Count',
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
                 ),
-              ),
-            ],
-          ),
+                Text('$counter'),
+                Row(
+                  children: [
+                    IconButton(
+                      iconSize: 30,
+                      onPressed: counterPlus,
+                      icon: const Icon(
+                        Icons.add_box_rounded,
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: counterMinus,
+                        icon: const Icon(Icons.remove_circle_rounded))
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
